abaa0287a742be78428cf3f8ef50419b5e35530e
Matt Lane <lane.matthew@gmail.com>
2016-07-07 14:31:05 -0700

    random scatterplot point working

--- js/scatterplot.js
+++ js/scatterplot.js
@@ -4,20 +4,33 @@ window.addEventListener('DOMContentLoaded', function() {
   var h = 400;
   var dataSet = []
   var pointList = d3.select("#point-list");
+  var x = d3.select('#x')
+  var y = d3.select('#y')
+  var r = d3.select('#r')
+  var color = d3.select('#color')
 
   var svg = d3.select('#graph')
               .append('svg')
               .attr('width', w)
               .attr('height', h);
 
-  d3.select('form').on('submit', function() {
-    d3.event.preventDefault();
-    var newData = { 
-      x: +this.x.value, 
-      y: +this.y.value,
-      r: +this.r.value,
-      color: this.color.value
-    };
+  d3.selectAll('input[type="button"]').on('click', function() {
+    var newData;
+    if (this.value === 'Plot') {
+      newData = { 
+        x: +this.x.value, 
+        y: +this.y.value,
+        r: +this.r.value,
+        color: this.color.value
+      };
+    } else {
+      newData = {
+        x: getRandom(-100, 100, 0.1),
+        y: getRandom(-100, 100, 0.1),
+        r: getRandom(0, 25, 0.1),
+        color: getRandomHex()
+      };
+    }
     dataSet.push(newData);
     draw(dataSet, svg);
     updateList(pointList, newData);
@@ -60,6 +73,20 @@ window.addEventListener('DOMContentLoaded', function() {
 
     // TODO: remove old circles
     
+  }
+
+  function getRandom(min, max, step) {
+    num = (max - min) * Math.random() + min;
+    return Math.round(num / step) / (1 / step);
+  }
+
+  function getRandomHex() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
   }
 
   function updateList(list, data) {
