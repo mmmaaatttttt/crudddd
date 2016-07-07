d4c1278b6f5c4b3accbee793aa827c0c74d29b47
Matt Lane <lane.matthew@gmail.com>
2016-07-07 09:54:52 -0700

    adding basic scatterplotting

--- js/scatterplot.js
+++ js/scatterplot.js
@@ -1,5 +1,48 @@
 window.addEventListener('DOMContentLoaded', function() {
 
-  console.log('scatterplot!');
+  var w = 600;
+  var h = 400;
+  var dataSet = []
+
+  var svg = d3.select('#graph')
+              .append('svg')
+              .attr('width', w)
+              .attr('height', h);
+
+  d3.select('form').on('submit', function() {
+    d3.event.preventDefault();
+    var newData = { 
+      x: this.x.value, 
+      y: this.y.value,
+      r: this.r.value,
+      color: this.color.value
+    };
+    dataSet.push(newData);
+    draw(dataSet, svg);
+  });
+
+  function draw(data, el) {
+    // create scales
+    var xmin = d3.min(data, d => d.x);
+    var xmax = d3.max(data, d => d.x);
+    var ymin = d3.min(data, d => d.y);
+    var ymax = d3.max(data, d => d.y);
+    var xScale = d3.scaleLinear()
+                   .domain([xmin, xmax])
+                   .range([0, w])
+    var yScale = d3.scaleLinear()
+                   .domain([ymin, ymax])
+                   .range([h,0]) 
+
+    // draw circles
+    el.selectAll('circle')
+      .data(data)
+      .enter()
+      .append('circle')
+      .attr('cx', d => xScale(d.x))
+      .attr('cy', d => yScale(d.y))
+      .attr('r', d => d.r)
+      .attr('fill', d => d.color)
+  }
 
 });
